19dda6168051c98f72a51837765874b39f3dfc66
ErikLewis95 <37794378+ErikLewis95@users.noreply.github.com>
2022-11-22 13:19:01 +0700

    Login, keep login, verification (#3)

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,24 +1,18 @@
-import { useRef } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { login, logout } from "../redux/userSlice";
-
-import Axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useRef, useState } from "react";
 import {
-  Flex,
-  Box,
-  FormControl,
-  FormLabel,
   Input,
-  Stack,
   Button,
+  Container,
   Heading,
-  Text,
-  useColorModeValue,
+  FormLabel,
+  FormControl,
 } from "@chakra-ui/react";
-import background from "../assets/background-login.avif";
+import Axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "../redux/userSlice";
+import { useNavigate } from "react-router-dom";
 
-const url = "http://localhost:2000/lib/login";
+const url = "http://localhost:2000/users/login";
 
 export const LoginPage = () => {
   const { NIM } = useSelector((state) => state.userSlice.value);
@@ -27,8 +21,7 @@ export const LoginPage = () => {
   const inputNIM = useRef("");
   const inputPassword = useRef("");
   const dispatch = useDispatch();
-  // const [NIM, setNIM] = useState("");
-  // const [password, setPassword] = useState("");
+
 
   const onLogin = async (data) => {
     data.preventDefault();
@@ -55,72 +48,25 @@ export const LoginPage = () => {
     }
   };
 
-  const onLogout = async () => {
-    dispatch(logout());
-    localStorage.removeItem("token");
-  };
 
   return (
-    <Box
-      style={{ backgroundImage: `url(${background})`, backgroundSize: "cover" }}
-    >
-      <Flex minH={"100vh"} align={"center"} justify={"center"}>
-        <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
-          <Box
-            rounded={"lg"}
-            bg={useColorModeValue("white", "gray.700")}
-            //   bg="blue"
-            boxShadow={"lg"}
-            p={8}
-          >
-            <Stack spacing={4}>
-              <Heading fontSize={"4xl"}>Sign in to your account</Heading>
-              <Text fontSize={"lg"} color={"gray.600"}>
-                to enjoy all the knowledge 📚
-              </Text>
-              <form onSubmit={onLogin}>
-                <FormControl
-                // id="NIM"
-                // defaultValue={{ NIM }}
-                // onChange={(e) => setNIM(e.target.value)}
-                >
-                  <FormLabel>NIM</FormLabel>
-                  <Input type="number" placeholder="NIM" ref={inputNIM} />
-                </FormControl>
-                <FormControl
-                // id="password"
-                // defaultValue={{ password }}
-                // onChange={(e) => setPassword(e.target.value)}
-                >
-                  <FormLabel>Password</FormLabel>
-                  <Input
-                    type="password"
-                    placeholder="password"
-                    ref={inputPassword}
-                  />
-                </FormControl>
-                <Stack spacing={10}>
-                  <Stack
-                    direction={{ base: "column", sm: "row" }}
-                    align={"start"}
-                    justify={"space-between"}
-                  ></Stack>
-                  <Button
-                    bg={"blue.400"}
-                    color={"white"}
-                    _hover={{
-                      bg: "blue.500",
-                    }}
-                    type="submit"
-                  >
-                    Sign in
-                  </Button>
-                </Stack>
-              </form>
-            </Stack>
-          </Box>
-        </Stack>
-      </Flex>
-    </Box>
+    <Container bg="#38A169" w="300px" h="350px" mt={20}>
+      <Heading mb={10}>Login</Heading>
+      <form onSubmit={onLogin}>
+      <FormControl>
+        <FormLabel>NIM</FormLabel>
+        <Input type="number" placeholder="NIM" ref={inputNIM} />
+      </FormControl>
+      <FormControl>
+        <FormLabel>Password</FormLabel>
+        <Input type="password" placeholder="password" ref={inputPassword} />
+      </FormControl>
+      <Button mt={10} w="100%" onClick={onLogin} colorScheme="orange">
+        Login
+      </Button>
+      </form>
+    </Container>
   );
 };
+
+
