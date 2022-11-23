1a1bb36c18e0c62d40f1b4e4dffc0172bd2cd73b
Rahmandani Winarta <88223348+rahmandaniwinarta@users.noreply.github.com>
2022-11-23 16:02:24 +0700

    done

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useRef } from "react";
 import {
   Input,
   Button,
@@ -12,7 +12,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { login } from "../redux/userSlice";
 import { useNavigate } from "react-router-dom";
 
-const url = "http://localhost:2000/users/login";
+const url = "http://localhost:2000/lib/login";
 
 export const LoginPage = () => {
   const { NIM } = useSelector((state) => state.userSlice.value);
@@ -21,7 +21,6 @@ export const LoginPage = () => {
   const inputNIM = useRef("");
   const inputPassword = useRef("");
   const dispatch = useDispatch();
-
 
   const onLogin = async (data) => {
     data.preventDefault();
@@ -48,25 +47,22 @@ export const LoginPage = () => {
     }
   };
 
-
   return (
     <Container bg="#38A169" w="300px" h="350px" mt={20}>
       <Heading mb={10}>Login</Heading>
       <form onSubmit={onLogin}>
-      <FormControl>
-        <FormLabel>NIM</FormLabel>
-        <Input type="number" placeholder="NIM" ref={inputNIM} />
-      </FormControl>
-      <FormControl>
-        <FormLabel>Password</FormLabel>
-        <Input type="password" placeholder="password" ref={inputPassword} />
-      </FormControl>
-      <Button mt={10} w="100%" onClick={onLogin} colorScheme="orange">
-        Login
-      </Button>
+        <FormControl>
+          <FormLabel>NIM</FormLabel>
+          <Input type="number" placeholder="NIM" ref={inputNIM} />
+        </FormControl>
+        <FormControl>
+          <FormLabel>Password</FormLabel>
+          <Input type="password" placeholder="password" ref={inputPassword} />
+        </FormControl>
+        <Button mt={10} w="100%" onClick={onLogin} colorScheme="orange">
+          Login
+        </Button>
       </form>
     </Container>
   );
 };
-
-
